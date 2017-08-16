351b8bfbade458b05f5ddb83a0896488165a2ba5
Preet Dha <manpreetdha+github@gmail.com>
2017-08-16 15:37:49 -0700

    Create component for main header; call the component in the rails index view

---
+++ app/assets/javascripts/components/main_header.es6.jsx
@@ -0,0 +1,9 @@
+class MainHeader extends React.Component {
+  render () {
+    return (
+      <div>
+        <h2 className='main-header-title'>Jobs Currently Available:</h2>
+      </div>
+    )
+  }
+}
