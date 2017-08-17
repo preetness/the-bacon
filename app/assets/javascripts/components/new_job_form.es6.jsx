8989dcd2f4913e00fc222ba919f96dd0412b2f4b
Preet Dha <manpreetdha+github@gmail.com>
2017-08-17 16:33:25 -0700

    Add basic styling to form

--- app/assets/javascripts/components/new_job_form.es6.jsx
+++ app/assets/javascripts/components/new_job_form.es6.jsx
@@ -9,6 +9,13 @@ class NewJobForm extends React.Component {
         <textarea name='description' placeholder='Ruby on Rails experience, React knowledge a plus' />
         <label>Company Name</label>
         <input type='text' name='company' placeholder='The Bacon' />
+        <label>Job Type</label>
+        <select name='type'>
+          <option value='full-time'>Full-Time</option>
+          <option value='part-time'>Part-Time</option>
+          <option value='temporary'>Temporary</option>
+          <option value='internship'>Internship</option>
+        </select>
       </div>
     );
   }
